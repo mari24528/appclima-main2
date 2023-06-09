a91b5108acf6c99263c0e4a0ef6cab4c51fec8d1
Osmy0103 <122169146+Osmy0103@users.noreply.github.com>
2023-06-09 13:40:00 -0300

    Initial commit

---
+++ proyecto/src/App.js
@@ -0,0 +1,21 @@
+import './assets/css/App.css';
+import NavBar from './components/NavBar';
+import Weatherapi from './components/Weatherapi';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+
+      <Weatherapi/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
